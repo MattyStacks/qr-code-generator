0cab2270c091bd9a5ea7ce372c9437d914cec45c
Matt Therault <matthew.therault@gmail.com>
2023-03-09 22:31:57 -0500

    Finished first version of the codebase for the generator.

--- src/components/QRCodeGenerator.vue
+++ src/components/QRCodeGenerator.vue
@@ -3,17 +3,30 @@
     <h1 class="qr-code-generator__title">QR Code Generator</h1>
     <form @submit.prevent="generateQRCode" class="qr-code-generator__form">
       <label for="url" class="qr-code-generator__label">Enter URL:</label>
-      <input type="text" id="url" v-model="url" required class="qr-code-generator__input">
-      <button type="submit" class="qr-code-generator__button">Generate QR Code</button>
+      <input
+        type="text"
+        id="url"
+        v-model="url"
+        required
+        class="qr-code-generator__input"
+      />
+      <button type="submit" class="qr-code-generator__button">
+        Generate QR Code
+      </button>
     </form>
     <div v-if="qrCode" class="qr-code-generator__result">
       <h2 class="qr-code-generator__subtitle">QR Code:</h2>
-      <img :src="qrCode" alt="QR Code" class="qr-code-generator__image">
+      <img :src="qrCode" alt="QR Code" class="qr-code-generator__image" />
       <div class="qr-code-generator__actions">
-        <button @click="downloadQRCode" class="qr-code-generator__button">Download QR Code</button>
-        <button @click="copyQRCode" class="qr-code-generator__button">Copy QR Code</button>
+        <button @click="downloadQRCode" class="qr-code-generator__button">
+          Download QR Code
+        </button>
+        <button @click="copyQRCode" class="qr-code-generator__button">
+          Copy QR Code
+        </button>
       </div>
     </div>
+    <div v-if="notification" :class="{ notification: true, show: showNotification }">{{ notification }}</div>
   </div>
 </template>
 
@@ -25,7 +38,8 @@ export default {
   data() {
     return {
       url: '',
-      qrCode: null
+      qrCode: null,
+      notification: ''
     }
   },
   methods: {
@@ -43,6 +57,7 @@ export default {
       link.download = 'qr-code.png'
       link.href = this.qrCode
       link.click()
+      this.showNotification('QR Code downloaded successfully')
     },
     copyQRCode() {
       const canvas = document.createElement('canvas')
@@ -55,11 +70,24 @@ export default {
         context.drawImage(img, 0, 0)
         canvas.toBlob(blob => {
           navigator.clipboard.write([new ClipboardItem({ 'image/png': blob })])
+            .then(() => {
+              this.showNotification('QR Code copied to clipboard')
+              this.showNotification = true;
+              setTimeout(() => {
+        this.showNotification = false;
+      }, 1500);
+            })
             .catch(error => {
               console.error(error)
             })
         })
       }
+    },
+    showNotification(message) {
+      this.notification = message
+      setTimeout(() => {
+        this.notification = ''
+      }, 1500)
     }
   }
 }
@@ -150,5 +178,24 @@ export default {
   .qr-code-generator__image {
     max-width: 80%; /* Double the maximum width for larger screens */
   }
+}
+.notification {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 9999;
+  background-color: #5cb85c;
+  color: white;
+  padding: 10px 20px;
+  border-radius: 5px;
+  font-size: 16px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+  opacity: 0;
+  transition: opacity 0.3s ease-in-out;
+}
+
+.notification.show {
+  opacity: 1;
 }
 </style>
